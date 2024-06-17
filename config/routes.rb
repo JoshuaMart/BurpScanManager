449f8f9bd1e2f9ee9b4a49a106e5a308b6347cf8
MRTSec <contact@jomar.fr>
2024-06-17 18:52:19 +0200

    Add sidekiq web

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   get 'users/index'
   devise_for :users, skip: %i[registrations passwords]
 
   # Defines the root path route ("/")
   root 'home#index'
+
+  authenticate :user do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 
   resources :users
   
