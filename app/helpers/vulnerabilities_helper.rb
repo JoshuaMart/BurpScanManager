7b851404c6593e3846234e69e10218901a560e58
MRTSec <contact@jomar.fr>
2024-06-18 20:46:05 +0200

    Improve vulnerabilities view

--- app/helpers/vulnerabilities_helper.rb
+++ app/helpers/vulnerabilities_helper.rb
@@ -2,4 +2,14 @@
 
 # VulnerabilitiesHelper
 module VulnerabilitiesHelper
+	def vulnerability_class(status)
+    case status
+    when 'high'
+      'danger'
+    when 'medium'
+      'success'
+    else
+      'secondary'
+    end
+  end
 end
